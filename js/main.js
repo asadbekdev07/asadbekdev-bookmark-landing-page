361a4354364104aed4774bef1f929da7ec0d904f
asadbekdev07 <futureprotrader1007@gmail.com>
2024-03-19 12:54:52 +0500

    Fix BEM naming errors in features tabpanels

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const elsTabsItem = document.querySelectorAll(".tabs__item");
 const elsTabLink = document.querySelectorAll(".js-tab-link");
-const elsTabsPanel = document.querySelectorAll(".tabs__panel");
+const elsTabsPanel = document.querySelectorAll(".tabspanel");
 
 function deactivateTabsItems() {
   elsTabsItem.forEach(function (elTabsItem) {
@@ -10,7 +10,7 @@ function deactivateTabsItems() {
 
 function deactivateTabsPanels() {
   elsTabsPanel.forEach(function (elTabsPanel) {
-    elTabsPanel.classList.remove("tabs__panel--active");
+    elTabsPanel.classList.remove("tabspanel--active");
   });
 }
 
@@ -31,6 +31,6 @@ elsTabLink.forEach(function (elTabLink) {
     // Show active tab panel
     // const elTargetPanel = document.querySelector(`#${elTabLink.href.split("#")[1]}`);
     const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
-    elTargetPanel.classList.add("tabs__panel--active");
+    elTargetPanel.classList.add("tabspanel--active");
   });
 });
