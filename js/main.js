1dca0e3aa2f8bccb23a6a466c46dc4570a84fa6c
asadbekdev07 <futureprotrader1007@gmail.com>
2024-03-19 12:32:55 +0500

    Make feature tabs work

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const elsTabsItem = document.querySelectorAll(".tabs__item");
+const elsTabLink = document.querySelectorAll(".js-tab-link");
+const elsTabsPanel = document.querySelectorAll(".tabs__panel");
+
+function deactivateTabsItems() {
+  elsTabsItem.forEach(function (elTabsItem) {
+    elTabsItem.classList.remove("tabs__item--active");
+  });
+}
+
+function deactivateTabsPanels() {
+  elsTabsPanel.forEach(function (elTabsPanel) {
+    elTabsPanel.classList.remove("tabs__panel--active");
+  });
+}
+
+elsTabLink.forEach(function (elTabLink) {
+  elTabLink.addEventListener("click", function(evt) {
+    //Prevent page move
+    evt.preventDefault();
+
+    //Remove active class from tabs__item elements
+    deactivateTabsItems();
+
+    //Add active class current tabs__item
+    elTabLink.parentElement.classList.add("tabs__item--active");
+
+    // Remove active class from tabs__panel elements
+    deactivateTabsPanels();
+
+    // Show active tab panel
+    // const elTargetPanel = document.querySelector(`#${elTabLink.href.split("#")[1]}`);
+    const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
+    elTargetPanel.classList.add("tabs__panel--active");
+  });
+});
